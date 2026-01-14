24142c9b902fd66b8c227881d0a214524246e5f9
Daniel Matos Falcão <45146744+batmit@users.noreply.github.com>
2026-01-14 15:38:14 -0300

    Add files via upload

--- tp.c
+++ tp.c
@@ -8,6 +8,10 @@ int main(){
 
     for(int i = 0; i < n; i++){
         adicionaFila(fila, i);
+<<<<<<< HEAD
+
+=======
+>>>>>>> 559bc6887ca4873d7d67fe1c4e9fa28e975d0242
     }
     insertionSort(fila, 0);
 
